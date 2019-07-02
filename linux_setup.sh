0017d01a672e61a3b1d5f67d1d7913b156737421
André Rodrigues <andre.silva@actualsales.com.br>
2019-07-02 18:17:39 -0300

    Bash linux

---
+++ linux_setup.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Set locale and timezone
+sudo dpkg-reconfigure --frontend noninteractive tzdata
+sudo locale-gen en_US.UTF-8 pt_BR.UTF-8
+sudo update-locale LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8
+
+# Update system
+sudo apt-get update \
+    && sudo apt-get -y dist-upgrade \
+    && sudo apt-get autoclean \
+    && sudo apt-get autoremove \
+    && sudo apt-get clean
+
+# Utilities
+sudo apt-get install -y git openssh-client openssh-server tree htop curl
+sudo bash /etc/bash_completion
+
+# Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo usermod -aG docker $USER
+
+# Snaps
+sudo apt -y install snapd
+sudo snap install code --classic
+sudo snap install gitkraken
+#sudo snap install spotify
+
+# System
+sudo echo "fs.inotify.max_user_watches = 524288" > /etc/sysctl.d/99-custom.conf
+sudo sysctl -p --system
+
+echo "Please reboot your system"
