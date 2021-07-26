8952af938e5ec2b069c2b79e8afd67f5e03cb952
André Rodrigues <andre.rsilva96@hotmail.com>
2021-07-26 11:34:32 -0300

    Update theme.sh

--- theme.sh
+++ theme.sh
@@ -18,14 +18,11 @@ echo "If your ubuntu witch output try https://askubuntu.com/questions/1181896/wr
 echo "-------------------------------"
 
 echo "# Show git branch name
-force_color_prompt=yes
-color_prompt=yes
 parse_git_branch() {
  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
-if [ \"$color_prompt\" = yes ]; then
+if [ "$color_prompt" = yes ]; then
  PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\]$(parse_git_branch)\[\033[00m\]\$ '
 else
  PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
-fi
-unset color_prompt force_color_prompt" >> ~/.bashrc
+fi" >> ~/.bashrc
