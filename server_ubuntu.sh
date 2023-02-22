7254aeb8648c4bfe99f5f6b7ccc541db18701112
André Rodrigues <andre.rsilva96@hotmail.com>
2023-02-22 00:58:10 -0300

    php ext php8.1-gd

--- server_ubuntu.sh
+++ server_ubuntu.sh
@@ -59,7 +59,7 @@ sudo apt-get update
 
 # Install PHP
 sudo apt install -y php8.1-common php8.1-cli
-sudo apt install -y php8.1-fpm php8.1-mysql php8.1-mbstring php8.1-xml php8.1-zip php8.1-curl
+sudo apt install -y php8.1-fpm php8.1-mysql php8.1-mbstring php8.1-xml php8.1-zip php8.1-curl php8.1-gd
 
 # Install Composer
 curl -sS https://getcomposer.org/installer -o composer-setup.php
