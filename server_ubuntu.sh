0f294e92d92d460d0ac462255b02baee5bfa14d0
André Rodrigues <andre.rsilva96@hotmail.com>
2023-04-10 12:05:12 -0300

    Node and Redis

--- server_ubuntu.sh
+++ server_ubuntu.sh
@@ -57,7 +57,7 @@ sudo systemctl restart sshd
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 
-# Install PHP
+# Install PHP V8.1
 sudo apt install -y php8.1-common php8.1-cli
 sudo apt install -y php8.1-fpm php8.1-mysql php8.1-mbstring php8.1-xml php8.1-zip php8.1-curl php8.1-gd
 
@@ -69,3 +69,10 @@ HASH=`curl -sS https://composer.github.io/installer.sig`
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 
 sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+# Install Node V16
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+# Install Redis
+sudo apt install -y redis-server
